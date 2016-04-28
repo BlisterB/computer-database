efca1339742f32a72dfda79addea0820e7a3c3c5
BlisterBoy <mehdi.abelha@gmail.com>
2016-04-28 17:50:57 +0200

    Migrate on HikariCP

--- ServerComputerDatabase/src/main/java/com/excilys/computer_database/database/ConnectionDB.java
+++ ServerComputerDatabase/src/main/java/com/excilys/computer_database/database/ConnectionDB.java
@@ -1,10 +1,14 @@
 package com.excilys.computer_database.database;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 public class ConnectionDB {
+    private static HikariConfig config;
+    private static HikariDataSource datasource;
 
     static {
         try {
@@ -15,17 +19,28 @@ public class ConnectionDB {
     }
 
     // TODO : Passer à des fichiers de propreties
-    private static final String
-    URL = "jdbc:mysql://127.0.0.1:3306/computer-database-db?zeroDateTimeBehavior=convertToNull",
-    USER = "admincdb",
-    PASSWORD = "qwerty1234";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/computer-database-db?zeroDateTimeBehavior=convertToNull";
 
-    /** Return a Connection to the DB.
+    /**
+     * Return a Connection to the DB.
      * @return A Connection to the DB
      */
     public static Connection getConnection() {
+        // First use : initialize properties, just one time
+        if (config == null) {
+            synchronized (ConnectionDB.class) {
+                if (config == null) {
+                    config = new HikariConfig("hikari.properties");
+                    config.setJdbcUrl(URL);
+                    // Should we close the datasource ?
+                    datasource = new HikariDataSource(config);
+                }
+            }
+        }
+
+        // Obtain the connection via HikariCP (for blazing fast performances)
         try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+            return datasource.getConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
