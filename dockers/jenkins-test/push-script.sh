dce280f4aa675873b084d82d1747ca420bf94ce4
BlisterBoy <mehdi.abelha@gmail.com>
2016-05-13 17:51:02 +0200

    Docker : new structure

--- dockers/jenkins-test/push-script.sh
+++ dockers/jenkins-test/push-script.sh
@@ -1,5 +1,5 @@
 # Copy the project to the dind-test
-docker cp ServerComputerDatabase java-maven:/ServerComputerDatabase
-docker exec java-maven cp /hikari.properties /ServerComputerDatabase/ServerComputerDatabase/src/main/ressources/hikari.properties
-docker exec java-maven cp /hikari.properties /ServerComputerDatabase/ServerComputerDatabase/src/main/ressources/hikari.properties
-docker exec java-maven mvn test
+docker cp ServerComputerDatabase computer-db-java-maven:/ServerComputerDatabase
+docker cp /hikari-production.properties computer-db-java-maven:/ServerComputerDatabase/ServerComputerDatabase/src/main/ressources/hikari.properties
+docker cp /hikari-test.properties       computer-db-java-maven:/ServerComputerDatabase/ServerComputerDatabase/src/test/ressources/hikari.properties
+docker exec computer-db-java-maven mvn test
--- dockers/tests.sh
+++ dockers/tests.sh
@@ -1,21 +1,28 @@
+NETWORK="network-computer-db"
+JENKINS="computer-db-jenkins"
+JENKINS_REPO="mkhelifi/computer-db-jenkins:latest"
+MYSQL_TEST="computer-db-mysql-test"
+MYSQL_TEST_REPO="mkhelifi/computer-db-mysql-test:latest"
+JAVA_MAVEN="computer-db-java-maven"
+JAVA_MAVEN_REPO="mkhelifi/computer-db-java-maven:latest"
+
 # Network creation
-docker network rm jenkins-dind
-docker network create --subnet 172.18.0.0/16 --gateway=172.18.1.1 jenkins-dind
+docker network rm $NETWORK
+docker network create --subnet 172.18.0.0/16 --gateway=172.18.1.1 $NETWORK
 
-# DinD : occupy the ip:port  172.18.0.1:2375
-docker stop dind-test ; docker rm dind-test
-docker run --privileged --name dind-test --net jenkins-dind -d docker:1.11.1-dind
-docker exec -it dind-test docker network create --subnet 172.19.0.0/16 --gateway=172.19.1.1 mysql-java
+# Jenkins : occupy the ip:port  172.18.0.1:8080, accessible from localhost:8080
+docker stop $JENKINS ; docker rm $JENKINS
+docker pull $JENKINS_REPO
+docker run -d --name $JENKINS --net $NETWORK -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/jenkins/computer-db/jenkins_home:/var/jenkins_home $JENKINS_REPO
 
-# Jenkins : occupy the ip:port  172.18.0.2:8080, accessible from localhost:8080
-docker stop jenkins-test ; docker rm jenkins-test
-docker pull mkhelifi/jenkins-test
-docker run -d --name jenkins-test --net jenkins-dind -p 8080:8080 -p 50000:50000 -v $HOME/jenkins_home:/var/jenkins_home mkhelifi/jenkins-test:latest
+# Mysql : 172.18.0.2:8080
+docker stop $MYSQL_TEST ; docker rm $MYSQL_TEST
+docker run -d --name $MYSQL_TEST --net $NETWORK $MYSQL_TEST_REPO
 
-# Mysql
-#docker exec -it dind-test docker stop mysql-test-db ; docker exec -it dind-test docker rm mysql-test-db
-docker exec dind-test docker run -d --name mysql-test-db --net mysql-java mkhelifi/mysql-test-db:latest
+# Java+Maven : 172.18.0.3:8080
+docker stop $JAVA_MAVEN ; docker rm $JAVA_MAVEN
+docker run -dit --name $JAVA_MAVEN --net $NETWORK $JAVA_MAVEN_REPO
 
-# Java+Maven : will need to be restart to launch commands
-docker exec dind-test docker run -dti --name java-maven --net mysql-java mkhelifi/java-maven:latest
-# docker run --rm --link dind-test:docker --name java-maven     --net computer-db-test mkhelifi/java-maven
+# Mysql : 172.18.0.4:8080
+
+# Mysql : 172.18.0.5:8080
